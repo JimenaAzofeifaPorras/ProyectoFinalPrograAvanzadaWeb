8cc92155bff4f2664e956b524d0f57b6353af70b
andre <andres@abarrientoso.com>
2024-02-23 00:30:34 -0600

    Added DAL to every entity and merged with previous commit "20f603c7"

---
+++ BD_GestionPiscinas/DAL/Implementations/EmpleadoDALImpl.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class EmpleadoDALImpl : DALGenericoImpl<Empleado>, IEmpleadoDAL
+    {
+        public EmpleadoDALImpl(GestionPiscinasContext context) : base(context)
+        {
+  
+        }
+    }
+}
---
+++ BD_GestionPiscinas/DAL/Implementations/ServicioPiscinaDALImpl.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class ServicioPiscinaDALImpl : DALGenericoImpl<Servicio>, IServicioPiscinaDAL
+    {
+        public ServicioPiscinaDALImpl(GestionPiscinasContext context) : base(context)
+        {
+
+        }
+    }
+}
